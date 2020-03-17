0b2dfe8121ac882865202fb3588e6f60c613b7c2
gouxj <24172280@qq.com>
2020-03-17 11:04:49 +0800

    1111

--- lists/tests.py
+++ lists/tests.py
@@ -3,5 +3,5 @@ from django.test import TestCase
 
 class SmokeTest(TestCase):
     def test_bad_maths(self):
-        print('test_bad_maths')
+        print('test_bad_maths ok')
         self.assertEqual(1 + 1, 3)
